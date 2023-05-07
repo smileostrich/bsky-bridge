d3b23c8a8a69e74f720db34a2dc623d767240de7
smileostrich <ian.ilminmoon@gmail.com>
2023-05-08 01:06:14 +0900

    fix: Correctly handle date-time strings without timezone in custom deserializer
    - Refactor deserialize method in custom deserializer to handle date-time strings without timezone separately
    - Parse input string as LocalDateTime for date-time strings without timezone and convert to ZonedDateTime using system default timezone
    - Update ZonedDateTime parsing for date-time strings with timezone using DateTimeFormatter.ISO_OFFSET_DATE_TIME

--- src/main/java/com/ian/bskyapp/config/ZonedDateTimeDeserializer.java
+++ src/main/java/com/ian/bskyapp/config/ZonedDateTimeDeserializer.java
@@ -5,20 +5,25 @@ import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class ZonedDateTimeDeserializer extends JsonDeserializer<ZonedDateTime> {
 
     @Override
     public ZonedDateTime deserialize(JsonParser parser, DeserializationContext context) throws IOException {
         String input = parser.getText();
+        ZonedDateTime zonedDateTime;
 
-        if (!input.endsWith("Z"))
-            input += "Z";
-
-        ZonedDateTime zonedDateTime = ZonedDateTime.parse(input)
-                .withZoneSameInstant(ZoneId.systemDefault());
+        if (!input.endsWith("Z")) {
+            LocalDateTime localDateTime = LocalDateTime.parse(input);
+            zonedDateTime = localDateTime.atZone(ZoneId.systemDefault());
+        } else {
+            zonedDateTime = ZonedDateTime.parse(input, DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+                    .withZoneSameInstant(ZoneId.systemDefault());
+        }
 
         return zonedDateTime;
     }
